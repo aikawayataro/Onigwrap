318ff797aa3e1149544db9fb6982ab046d7717ee
Aikawa Yataro <aikawayataro@protonmail.com>
2024-08-07 03:46:42 +0000

    Fix indentation mix.

--- onigwrap/onigwrap.c
+++ onigwrap/onigwrap.c
@@ -2,21 +2,21 @@
 
 regex_t *onigwrap_create(char *pattern, int len, int ignoreCase, int multiline)
 {
-	regex_t *reg;
+    regex_t *reg;
 
-	OnigErrorInfo einfo;
+    OnigErrorInfo einfo;
 
-	OnigOptionType onigOptions = ONIG_OPTION_NONE | ONIG_OPTION_CAPTURE_GROUP;
+    OnigOptionType onigOptions = ONIG_OPTION_NONE | ONIG_OPTION_CAPTURE_GROUP;
 
-	if (ignoreCase == 1)
-		onigOptions |= ONIG_OPTION_IGNORECASE;
+    if (ignoreCase == 1)
+        onigOptions |= ONIG_OPTION_IGNORECASE;
 
-	if (multiline == 1)
-		onigOptions |= ONIG_OPTION_MULTILINE;
+    if (multiline == 1)
+        onigOptions |= ONIG_OPTION_MULTILINE;
 
-	OnigUChar *stringStart = (OnigUChar*) pattern;
-	OnigUChar *stringEnd   = (OnigUChar*) pattern + len;
-	
+    OnigUChar *stringStart = (OnigUChar*) pattern;
+    OnigUChar *stringEnd   = (OnigUChar*) pattern + len;
+    
     int res = onig_new(
         &reg,
         stringStart,
@@ -26,58 +26,58 @@ regex_t *onigwrap_create(char *pattern, int len, int ignoreCase, int multiline)
         ONIG_SYNTAX_DEFAULT,
         &einfo);
 
-	return reg;
+    return reg;
 }
 
 OnigRegion* onigwrap_region_new()
 {
-	return onig_region_new();
+    return onig_region_new();
 }
 
 void onigwrap_region_free(OnigRegion *region)	
 {
-	onig_region_free(region, 1);
+    onig_region_free(region, 1);
 }
 
 void onigwrap_free(regex_t *reg)
 {
-	onig_free(reg);
+    onig_free(reg);
 }
 
 int onigwrap_search(regex_t *reg, char *charPtr, int offset, int length, OnigRegion *region)
 {
-	OnigUChar *stringStart  = (OnigUChar*) charPtr;
-	OnigUChar *stringEnd    = (OnigUChar*) (charPtr + length);
-	OnigUChar *stringOffset = (OnigUChar*) (charPtr + offset);
-	OnigUChar *stringRange  = (OnigUChar*) stringEnd;
+    OnigUChar *stringStart  = (OnigUChar*) charPtr;
+    OnigUChar *stringEnd    = (OnigUChar*) (charPtr + length);
+    OnigUChar *stringOffset = (OnigUChar*) (charPtr + offset);
+    OnigUChar *stringRange  = (OnigUChar*) stringEnd;
 
-	int result = onig_search(reg, stringStart, stringEnd, stringOffset, stringRange, region, ONIG_OPTION_NONE);
-	return result;
+    int result = onig_search(reg, stringStart, stringEnd, stringOffset, stringRange, region, ONIG_OPTION_NONE);
+    return result;
 }
 
 int onigwrap_num_regs(OnigRegion *region)
 {
-	return region->num_regs;
+    return region->num_regs;
 }
 
 int onigwrap_pos(OnigRegion *region, int nth)
 {
-	if (nth < region->num_regs)
-	{
-		int result = region->beg[nth];
-		if (result < 0)
-			return result;
-		return result >> 1;
-	}
-	return -1;
+    if (nth < region->num_regs)
+    {
+        int result = region->beg[nth];
+        if (result < 0)
+            return result;
+        return result >> 1;
+    }
+    return -1;
 }
 
 int onigwrap_len(OnigRegion *region, int nth)
 {
-	if (nth < region->num_regs)
-	{
-		int result = region->end[nth] - region->beg[nth];
-		return result >> 1;
-	}
-	return -2;
+    if (nth < region->num_regs)
+    {
+        int result = region->end[nth] - region->beg[nth];
+        return result >> 1;
+    }
+    return -2;
 }
